1255f89d8e37f4f7e3d08ff098e2160ecaa3ae7f
MeshachB <MeshachCBrown@gmail.com>
2025-09-22 22:46:55 -0400

    Completed exercise 1

---
+++ app.js
@@ -0,0 +1,20 @@
+//Exercise 1 
+
+const maxOfTwoNumbers = (x,y) => {
+    if (x >= y) {
+        return x;
+    } else {
+        return y;
+    }
+} 
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9)); 
+
+
+
+
+//Exercise 2 
+
+function isAdult(num){
+if (num) >= 18 {
+    
+}
